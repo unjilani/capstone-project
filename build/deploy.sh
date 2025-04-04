380993478941d47fd784c5945c3e99ad563fd618
unjilani <unjilani@yahoo.com>
2025-04-04 13:44:20 +0300

    Batch DO-WE-E-B4

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,9 +4,9 @@
 set -e  
 
 # Configuration Variables
-IMAGE_NAME=${IMAGE_NAME:-"my_app"}  # Default Docker image name
+IMAGE_NAME=${IMAGE_NAME:-"MyApp"}  # Default Docker image name
 TAG=${TAG:-"latest"}                # Default tag
-DOCKERFILE="Dockerfile"             # Dockerfile name
+DOCKERFILE="dockerfile"             # Dockerfile name
 PORT=8080                            # Change to required port
 
 # Build Docker Image
@@ -14,7 +14,7 @@ echo "Building Docker image: $IMAGE_NAME:$TAG"
 docker build -t "$IMAGE_NAME:$TAG" -f "$DOCKERFILE" .
 
 echo " deploy Running new container..."
-  docker run -d --name $IMAGE_NAME -p $PORT:$PORT "$IMAGE_NAME:$TAG"
+  docker run -d --name "$IMAGE_NAME" -p $PORT:$PORT "$IMAGE_NAME:$TAG"
 
 echo "Deployment complete!"
 
